67867725596bbe2d1052e4cb65e7bd607403651e
Peter.Li <1406445753@qq.com>
2016-10-16 23:14:35 +0800

    file service download

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -17,11 +17,46 @@ angular.module('starter.controllers', [])
             browseClass: "btn btn-primary", //按钮样式         
 			previewFileIcon: "<i class='glyphicon glyphicon-king'></i>", 
             msgFilesTooMany: "选择上传的文件数量({n}) 超过允许的最大数值{m}！",
-            uploadExtraData: { secureLevel: 1 },
+            uploadExtraData: { secureLevel: 0 },
 		});
 	};
 	
 	initFileInput("file-upload", "http://localhost:8100/OxygenWeb/commonService/fileUploadSingle");
+	
+	
+	$("#downloadTypeZero").click(function(){
+		
+		var getResourceOwnerAllOption = {
+			type: 'POST',
+			url: '/OxygenWeb/commonService/fileDownloadSingle',
+			data: { id: "cb05d2f5-026e-4d23-8f5d-b6f6ce9f5d0e" },
+			success: function(data){
+			},
+			error: function(XMLHttpRequest, textStatus, errorThrown){
+			},
+			timeout: function(data){
+			}
+		};
+		//initialize the page
+		$.ajax(getResourceOwnerAllOption);
+	});
+	
+	$("#downloadTypeOne").click(function(){
+		
+		var getResourceOwnerAllOption = {
+			type: 'POST',
+			url: '/OxygenWeb/commonService/fileDownloadSingle',
+			data: { id: "47683794-40ab-4977-bf04-dd1c87035453" },
+			success: function(data){
+			},
+			error: function(XMLHttpRequest, textStatus, errorThrown){
+			},
+			timeout: function(data){
+			}
+		};
+		//initialize the page
+		$.ajax(getResourceOwnerAllOption);
+	});
 })
 
 .controller('ChatsCtrl', function($scope, Chats) {
