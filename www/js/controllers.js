3081c91f7cd6aab6ee17cf7e48efb890ca29c2b0
Peter.Li <1406445753@qq.com>
2016-10-16 16:15:22 +0800

    update for file upload

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,28 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope) {
+	
+	
+	function initFileInput(ctrlName, uploadUrl) {    
+		var control = $('#' + ctrlName); 
+
+		control.fileinput({
+			language: 'zh', //设置语言
+			uploadUrl: uploadUrl, //上传的地址
+			allowedFileExtensions : ['jpg', 'png','gif'],//接收的文件后缀
+            maxFileCount: 100,
+            enctype: 'multipart/form-data',
+			showUpload: false, //是否显示上传按钮
+			showCaption: false,//是否显示标题
+            browseClass: "btn btn-primary", //按钮样式         
+			previewFileIcon: "<i class='glyphicon glyphicon-king'></i>", 
+            msgFilesTooMany: "选择上传的文件数量({n}) 超过允许的最大数值{m}！",
+            uploadExtraData: { secureLevel: 1 },
+		});
+	};
+	
+	initFileInput("file-upload", "http://localhost:8100/OxygenWeb/commonService/fileUploadSingle");
+})
 
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
--- www/lib/main.js
+++ www/lib/main.js
@@ -9,14 +9,15 @@ requirejs.config({
     paths: {
 		jquery:'lib/jquery-3.1.0.min',
         bootstrap: '/lib/bootstrap-3.3.7-dist/js/bootstrap.min',
-		bootstrapDatepicker: '/lib/bootstrap-datetimepicker-master/js/bootstrap-datetimepicker.min'
+		bootstrapDatepicker: '/lib/bootstrap-datetimepicker-master/js/bootstrap-datetimepicker.min',
+		bootstrapFileinput: '/lib/bootstrap-fileinput/js/fileinput.min'
     }, 
-	shim: { "bootstrap": { deps: ["jquery"], exports: "bootstrapDatepicker" } }
+	shim: { "bootstrap": { deps: ["jquery"], exports: ["bootstrapDatepicker","bootstrapFileinput"] } }
 });
 
 // Start the main app logic.
-requirejs(['jquery','bootstrap','bootstrapDatepicker'],
+requirejs(['jquery','bootstrap','bootstrapDatepicker','bootstrapFileinput'],
 function($) {
 	console.log("hello requirejs.");
-	$('#datetimepicker').datetimepicker('show');
+	//$('#datetimepicker').datetimepicker('show');
 });
